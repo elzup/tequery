e104add76b23191ae5187a251ccd18ed98f9505c
elzup <guild0105@gmail.com>
2022-05-13 16:17:11 +0900

    character check to func

--- src/suggester/index.ts
+++ src/suggester/index.ts
@@ -1,9 +1,33 @@
-type CharactType = 'multiline' | 'csvLike' | 'tsvLike'
-
 // TODO: commandLike '$ '
 // TODO: hasColon
 
 type Characts = Record<CharactType, boolean>
+type Suggestion = {
+  key: CharactType
+  check: (part: {
+    text: string
+    line1: string | null
+    lines: string[]
+  }) => boolean
+}
+
+type CharactType = 'multiline' | 'csvLike' | 'tsvLike'
+const suggests: Suggestion[] = [
+  {
+    key: 'multiline',
+    check: ({ lines }) => lines.length > 1,
+  },
+  {
+    key: 'csvLike',
+    check: ({ line1 }) => (line1 ?? '').split(',').length >= 3,
+  },
+  {
+    key: 'tsvLike',
+    check: ({ line1 }) => (line1 ?? '').split('\t').length >= 3,
+  },
+]
+// const charactTypes = suggests.map((v) => v.key)
+
 export const getCharacts = (text: string): Characts => {
   const characts: Characts = {
     multiline: false,
@@ -11,13 +35,12 @@ export const getCharacts = (text: string): Characts => {
     tsvLike: false,
   }
 
-  if (text.includes('\n')) characts.multiline = true
-  const firstLine = text.split('\n')[0]
+  const lines = text.split('\n')
+  const line1 = lines[0] || null
 
-  if (firstLine) {
-    if (firstLine.split(',').length >= 3) characts.csvLike = true
-    if (firstLine.split('\t').length >= 3) characts.tsvLike = true
-  }
+  suggests.forEach((s) => {
+    characts[s.key] = s.check({ text, line1, lines })
+  })
 
   return characts
 }
