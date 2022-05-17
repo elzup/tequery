a581a381b40913068b1f00675fcbfb108c41a2c1
elzup <guild0105@gmail.com>
2022-05-17 23:56:51 +0900

    add pack test

--- src/__test__/suggester.test.ts
+++ src/__test__/suggester.test.ts
@@ -17,54 +17,107 @@ test('getAttrs', () => {
   expect(getAttrsStr('a,b,c,d,e').cellLike).toBe(true)
 })
 
-test('suggester', () => {
-  const suggestResult = suggester('a,b,c,d,e').map((v) => [
-    v.dict.name,
-    v.point,
-  ])
+describe('suggester', () => {
+  it('basic sample', () => {
+    const suggestResult = suggester('a,b,c,d,e').map((v) => [
+      v.dict.name,
+      v.point,
+    ])
 
-  expect(suggestResult).toMatchInlineSnapshot(`
-    Array [
+    expect(suggestResult).toMatchInlineSnapshot(`
+          Array [
+            Array [
+              "shiftl",
+              100,
+            ],
+            Array [
+              "shiftr",
+              100,
+            ],
+            Array [
+              "cq",
+              50,
+            ],
+            Array [
+              "len",
+              0,
+            ],
+            Array [
+              "count",
+              0,
+            ],
+            Array [
+              "lineNum",
+              0,
+            ],
+            Array [
+              "ln",
+              0,
+            ],
+            Array [
+              "pack",
+              -100,
+            ],
+            Array [
+              "json",
+              -100,
+            ],
+            Array [
+              "jsonf",
+              -100,
+            ],
+          ]
+      `)
+  })
+  it('basic sample pack', () => {
+    const suggestResult = suggester('a\n\nb\nc\nd\n\ne\n\nf').map((v) => [
+      v.dict.name,
+      v.point,
+    ])
+
+    expect(suggestResult).toMatchInlineSnapshot(`
       Array [
-        "shiftl",
-        100,
-      ],
-      Array [
-        "shiftr",
-        100,
-      ],
-      Array [
-        "cq",
-        50,
-      ],
-      Array [
-        "len",
-        0,
-      ],
-      Array [
-        "count",
-        0,
-      ],
-      Array [
-        "lineNum",
-        0,
-      ],
-      Array [
-        "ln",
-        0,
-      ],
-      Array [
-        "pack",
-        -100,
-      ],
-      Array [
-        "json",
-        -100,
-      ],
-      Array [
-        "jsonf",
-        -100,
-      ],
-    ]
-  `)
+        Array [
+          "pack",
+          90,
+        ],
+        Array [
+          "cq",
+          50,
+        ],
+        Array [
+          "len",
+          0,
+        ],
+        Array [
+          "count",
+          0,
+        ],
+        Array [
+          "lineNum",
+          0,
+        ],
+        Array [
+          "ln",
+          0,
+        ],
+        Array [
+          "shiftl",
+          -100,
+        ],
+        Array [
+          "shiftr",
+          -100,
+        ],
+        Array [
+          "json",
+          -100,
+        ],
+        Array [
+          "jsonf",
+          -100,
+        ],
+      ]
+    `)
+  })
 })
